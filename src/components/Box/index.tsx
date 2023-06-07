d2a4b757860691f8604a454e99dcfea8cd44e38f
Giovanne Mendonca <giovannemendonca@outlook.com>
2023-06-06 23:02:07 -0300

    bug-fix: box-layout

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -4,5 +4,10 @@ export const Box = styled('div', {
   padding: '$16',
   borderRadius: '$md',
   backgroundColor: '$gray800',
-  border: 'solid 1px $gray600'
+  border: 'solid 1px $gray600',
+
+  '@media (max-width: 620px)': {
+    padding: '$6'
+  }
+
 })
